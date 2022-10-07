3dc46bbf1d2aa9d08db3a155ae03c9d06b93a3b1
abiramen <7523422+abiramen@users.noreply.github.com>
2022-10-07 12:33:28 +1100

    week04-f13b: starter code

---
+++ week04-f13b/flag.s
@@ -0,0 +1,58 @@
+N_ROWS = 6
+N_COLS = 12
+
+main:
+
+	# Locals:
+	#	- $t0: int row
+	#	- $t1: int col
+	#	- $t2: temporary result
+
+main__for_row_init:
+	li	$t0, 0					# int row = 0;
+
+main__for_row_cond:
+	bge	$t0, N_ROWS, main__for_row_end		# if (row >= N_ROWS) goto main__for_row_end;
+
+main__for_row_body:
+
+main__for_col_init:
+	li	$t1, 0					# int col = 0;
+
+main__for_col_cond:
+	bge	$t1, N_COLS, main__for_col_end		# if (col >= N_COLS) goto main__for_col_end;
+
+main__for_col_body:
+	
+	# TODO: array calculation
+
+	li      $v0, 11					# syscall 11: print_char
+	syscall						# printf("%c", flag[row][col]);
+
+
+main__for_col_step:
+	addi	$t1, $t1, 1				# col++;
+	j	main__for_col_cond
+
+
+main__for_col_end:
+	li	$v0, 11					# syscall 11: print_char
+	li	$a0, '\n'				#
+	syscall						# putchar('\n');
+
+main__for_row_step:
+	addi	$t0, $t0, 1		# i++;
+	j	main__for_row_cond
+
+main__for_row_end:
+	li      $v0, 0
+	jr      $ra			# return 0;
+	.data
+flag:
+
+	.byte '#', '#', '#', '#', '#', '.', '.', '#', '#', '#', '#', '#',
+	.byte '#', '#', '#', '#', '#', '.', '.', '#', '#', '#', '#', '#',
+	.byte '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.',
+	.byte '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.',
+	.byte '#', '#', '#', '#', '#', '.', '.', '#', '#', '#', '#', '#',
+	.byte '#', '#', '#', '#', '#', '.', '.', '#', '#', '#', '#', '#'
